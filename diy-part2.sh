e4804399271049e708756fd33ee2ad9c91839c03
rtds <61504899+ligehhh@users.noreply.github.com>
2022-02-22 18:16:09 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -8,7 +8,18 @@
 # https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
-#
-
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.100.3/g' package/base-files/files/bin/config_generate #修改默认ip
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile   # 选择argon为默认主题
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-nginx/Makefile   # 选择argon为默认主题
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-ssl-nginx/Makefile   # 选择argon为默认主题
+rm -rf  feeds/luci/themes/luci-theme-argon   # 删除自带argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon   # 替换新版argon
+sed -i '/<tr><td width="33%"><%:CPU usage/a <tr><td width="33%"><%:Compiler author%></td><td>QQ 912466913</td></tr>' package/lean/autocore/files/x86/index.htm #添加编译作者
+sed -i 's/OpenWrt/Nianwrt/g' package/base-files/files/bin/config_generate #修改主机名
+sed -i '5a\msgid "Compiler author"' feeds/luci/modules/luci-base/po/zh-cn/base.po #添加汉化
+sed -i '6a\msgstr "编译作者"' feeds/luci/modules/luci-base/po/zh-cn/base.po #添加汉化
+sed -i '7a \\' feeds/luci/modules/luci-base/po/zh-cn/base.po #添加汉化
+sed -i '/<tr><td width="33%"><%:Compiler author/a <tr><td width="33%"><%:Firmware Update%></td><td><a target="_blank" href="https://github.com/ligehhh/openwrt/releases">https://github.com/ligehhh/openwrt/releases</a></td></tr>' package/lean/autocore/files/x86/index.htm #添加固件更新地址
+sed -i '8a\msgid "Firmware Update"' feeds/luci/modules/luci-base/po/zh-cn/base.po #添加汉化
+sed -i '9a\msgstr "固件更新"' feeds/luci/modules/luci-base/po/zh-cn/base.po #添加汉化
+sed -i '10a \\' feeds/luci/modules/luci-base/po/zh-cn/base.po #添加汉化
