60527901f7239e42a7a603de4f7f18af79104fda
rtds <61504899+ligehhh@users.noreply.github.com>
2022-08-21 11:21:32 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -23,11 +23,11 @@ sed -i '/<tr><td width="33%"><%:Compiler author/a <tr><td width="33%"><%:Firmwar
 sed -i '8a\msgid "Firmware Update"' feeds/luci/modules/luci-base/po/zh-cn/base.po #添加汉化
 sed -i '9a\msgstr "固件更新"' feeds/luci/modules/luci-base/po/zh-cn/base.po #添加汉化
 sed -i '10a \\' feeds/luci/modules/luci-base/po/zh-cn/base.po #添加汉化
-sed -i '/<tr><td width="33%"><%:CPU usage/a <tr><td width="33%"><%:oopp%></td><td > <iframe width="700" height="100" frameborder="0" scrolling="no" hspace="0" src="//ip.skk.moe/simple"></iframe> #添加测速IP地址
+sed -i '/<tr><td width="33%"><%:CPU usage/a <tr><td width="33%"><%:oopp%></td><td > <iframe width="700" height="100" frameborder="0" scrolling="no" hspace="0" src="//ip.skk.moe/simple"></iframe>' package/lean/autocore/files/x86/index.htm #添加测速IP地址
 sed -i '11a\msgid "oopp"' feeds/luci/modules/luci-base/po/zh-cn/base.po #添加汉化
 sed -i '12a\msgstr "网络IP地址"' feeds/luci/modules/luci-base/po/zh-cn/base.po #添加汉化
 sed -i '13a \\' feeds/luci/modules/luci-base/po/zh-cn/base.po #添加汉化
-sed -i '/<tr><td width="33%"><%:Firmware Update/a <tr><td width="10%"><%:Urban weather%></td><td > <iframe width="900" height="120" frameborder="0" scrolling="no" hspace="0" src="//i.tianqi.com/?c=code&a=getcode&id=22&py= &icon=1"></iframe> #添加天气
+sed -i '/<tr><td width="33%"><%:Firmware Update/a <tr><td width="10%"><%:Urban weather%></td><td > <iframe width="900" height="120" frameborder="0" scrolling="no" hspace="0" src="//i.tianqi.com/?c=code&a=getcode&id=22&py= &icon=1"></iframe>' package/lean/autocore/files/x86/index.htm #添加天气
 sed -i '14a\msgid "Urban weather"' feeds/luci/modules/luci-base/po/zh-cn/base.po #添加汉化
 sed -i '15a\msgstr "最近天气"' feeds/luci/modules/luci-base/po/zh-cn/base.po #添加汉化
 sed -i '16a \\' feeds/luci/modules/luci-base/po/zh-cn/base.po #添加汉化
